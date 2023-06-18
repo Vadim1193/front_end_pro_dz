8dc7b80d7048efdabbdc98ddbb6490e387e2f479
Filimochuk Vadim <kievbasss@gmail.com>
2023-06-19 01:02:06 +0300

    update dz_47

--- dz_47/js/main.js
+++ dz_47/js/main.js
@@ -1,36 +1,45 @@
 
-function createNumbers() {    
-    const tableElem = document.createElement('table');
-    tableElem.classList.add('elem_table');
-    const wrapperTab = document.querySelector('.wrapper_table');
-
+function createTable() {   
+    const wrapperTab = document.querySelector('.wrapper_table'); 
     const condition = 10;
-    let num = 1;
 
-    for (let i = 0; i < condition; i++) {
-        const row = document.createElement('tr');
-    
-        for (let j = 0; j < condition; j++) {
-            const index = i * condition + j;
-            setTimeout(() => {
-                const cell = document.createElement('td');
-                cell.classList.add('td');
-                cell.innerText = num;
+    function getTableElem() {
+        const tableElem = document.createElement('table');
+        tableElem.classList.add('elem_table');
+        return tableElem;
+    }
+
+    const createRow = () => document.createElement('tr');
+
+    function createCell(num) {
+        const cell = document.createElement('td');
+        cell.classList.add('td');
+        cell.innerText = num;
+        return cell;
+    } 
+
+    function fillTable() {
+        const myTable = getTableElem();
+        const fragment = document.createDocumentFragment();
+        let num = 1;
+
+        for (let i = 0; i < condition; i++) {
+            const row = createRow();
+
+            for (let j = 0; j < condition; j++) {
+                const cell = createCell(num);
                 row.append(cell);
                 num++;
-            }, index * 100);
+            }
+            fragment.append(row);
         }
-        tableElem.append(row);
+        myTable.append(fragment);
+        wrapperTab.append(myTable);
     }
-    wrapperTab.append(tableElem);
-};
+    setTimeout(fillTable, 2000);
+}
 
-window.onload = createNumbers;
-
-
+window.onload = createTable;
 
 
-
-
-
-
+  
